faa7cd60ec5bc603f45d351ac88c3a61049fee1d
asir.linux <asir.linux@gmail.com>
2024-11-04 16:11:55 +0000

    Your descriptive commit message

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "example_1" {
   ami           = "ami-0ddc798b3f1a5117e"
   instance_type = "t2.micro"
   tags = {

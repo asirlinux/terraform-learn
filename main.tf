dfb8ad5f989c3dbb4ce7463c3c648530cd1851fb
asir.linux <asir.linux@gmail.com>
2024-11-05 19:23:18 +0530

    Updated _9 on instance name

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "example_3" {
   ami           = "ami-0ddc798b3f1a5117e"
   instance_type = "t2.micro"
   tags = {
-    Name = "ExampleInstance"
+    Name = "ExampleInstance_9"
   }
 }
